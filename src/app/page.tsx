e6ba3d9af81f34d869ce2d9598e7657bd345106c
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-10-10 22:17:49 +0530

    retest 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   console.log("who are you",process.env.BASE_URL);
   return (
    <div className="flex flex-col w-screen h-screen justify-center items-center bg-teal-100">
-    <Image src={`${process.env.BASE_URL}/images/UnderConstruction.jpg`} alt="UnderConstruction" className="flex-1 m-10 mt-20" width={500} height={500}/>
+    <Image src={`/one-million/images/UnderConstruction.jpg`} alt="UnderConstruction" className="flex-1 m-10 mt-20" width={500} height={500}/>
     <p className="text-black text-2xl m-10 flex-1">This Page is under construction. Please come back soon.</p>
    </div>
   );
