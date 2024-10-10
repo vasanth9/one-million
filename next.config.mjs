f1940b7c5c3898735b9fd5665c80d3449222feeb
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-10-10 16:31:53 +0530

    deployment setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath:"/one-million",
+    output:"export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
