5e3d3066a42add8f2c3741ff16b942501308a2d0
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-10-10 17:29:41 +0530

    d test 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div className="flex flex-col w-screen h-screen justify-center items-center bg-teal-100">
-    <Image src="/images/UnderConstruction.jpg" alt="UnderConstruction" className="flex-1 m-10" width={500} height={500}/>
+    <Image src="/images/UnderConstruction.jpg" alt="UnderConstruction" className="flex-1 m-10 mt-20" width={500} height={500}/>
     <p className="text-black text-2xl m-10 flex-1">This Page is under construction. Please come back soon.</p>
    </div>
   );
