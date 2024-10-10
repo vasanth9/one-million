089092e34927792638481bf00c1d3c38c71a40b2
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-10-10 22:15:33 +0530

    base url test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 export default function Home() {
+  console.log("who are you",process.env.BASE_URL);
   return (
    <div className="flex flex-col w-screen h-screen justify-center items-center bg-teal-100">
     <Image src={`${process.env.BASE_URL}/images/UnderConstruction.jpg`} alt="UnderConstruction" className="flex-1 m-10 mt-20" width={500} height={500}/>
